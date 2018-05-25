0310bd8fb01466ca1e393f42b2fabe1cc2c31cba
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-25 13:27:46 +0100

    Piano Class tests passing

--- src/main/java/Instruments/Piano.java
+++ src/main/java/Instruments/Piano.java
@@ -1,12 +1,19 @@
 package Instruments;
 
 public class Piano extends Instrument implements IPlay {
+    private int octanes;
 
-    public Piano(String type, InstrumentType instrumentType, MaterialType materialType, String colour, double buyingPrice,
-                 double sellingPrice, String brand, int octanes) {
+    public Piano(String type, InstrumentType instrumentType, MaterialType materialType, String colour, double buyingPrice, double sellingPrice, String brand, int octanes) {
         super(type, instrumentType, materialType, colour, buyingPrice, sellingPrice, brand);
+    this.octanes = octanes;
         }
 
+
+    public int getNoOfOctanes() {
+        return this.octanes;
+    }
+
+    ;
 
 
 }
---
+++ src/test/java/PianoTest.java
@@ -0,0 +1,22 @@
+import Instruments.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class PianoTest {
+
+    Piano piano;
+    IPlay iPlay;
+
+    @Before
+    public void before() {
+        piano = new Piano("Grand", InstrumentType.PERCUSSION, MaterialType.WOOD, "White", 2000.00, 2500.00, "Roland", 8);
+    }
+
+    @Test
+    public void canGetNoOfOctanes() {
+        assertEquals(8, piano.getNoOfOctanes());
+    }
+
+}
